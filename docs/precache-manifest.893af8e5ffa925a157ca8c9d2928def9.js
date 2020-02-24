c2f88dd6ccd607fa8a3e0dfb3a0fd797a5e0023b
Filippo Ferri <hello@filippoferri.co>
2020-02-24 10:27:37 +0100

    added docs

---
+++ docs/precache-manifest.893af8e5ffa925a157ca8c9d2928def9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b4499f64714ca6eb9146",
+    "url": "/js/index.b6a36098.js"
+  },
+  {
+    "revision": "3f4d2f707be2a23e4c3c",
+    "url": "/js/chunk-vendors.d9b64850.js"
+  },
+  {
+    "revision": "97f154950873785396ac",
+    "url": "/js/chunk-common.ab91f5b4.js"
+  },
+  {
+    "revision": "4d003392ebce0717850b",
+    "url": "/js/200.ad15a1ed.js"
+  },
+  {
+    "revision": "a8ff3eca3f75d5f77c89990a67b4ac81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f9393320af0fa5ccabb406693c0c97b",
+    "url": "/data/wheel.json"
+  },
+  {
+    "revision": "3f4d2f707be2a23e4c3c",
+    "url": "/css/chunk-vendors.354276a6.css"
+  },
+  {
+    "revision": "97f154950873785396ac",
+    "url": "/css/chunk-common.33595a8c.css"
+  },
+  {
+    "revision": "79e11b959082c6ff445dcc3d239c1685",
+    "url": "/200.html"
+  }
+];
